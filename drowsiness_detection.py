6c7ce9df81ff8f81935e39a6d727c987a2e979ed
Sourish Sarmah <sourishsarmah@gmail.com>
2019-02-21 21:19:56 +0530

    Face detection implemented

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -1,4 +1,5 @@
 import cv2
+from eye_detect import detectFace
 
 def main():
     cam = cv2.VideoCapture(0)
@@ -7,6 +8,9 @@ def main():
         _, frame = cam.read()
 
         cv2.imshow('Drowsiness Detector', frame)
+
+        f_points = detectFace(frame)
+        # print(f_points, sep='\n')
         
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
