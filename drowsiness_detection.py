87bd70598ff9998f2831506a1209aaa4b609ca0f
Sourish Sarmah <sourishsarmah@gmail.com>
2019-02-25 20:53:57 +0530

    drowsiness detection and alert added

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -4,34 +4,33 @@ from keras.models import load_model
 from model import cnnPreprocess
 from imutils import resize
 
+
 def main():
     cam = cv2.VideoCapture(0)
     model = load_model('eyeblink.hdf5')
 
-    # blink_count counts the total number of blinks
-    # close_count counts the consecutive close predictions
-    # mem_count is the counter for the previous loop
-    close_count = blink_count = mem_count = 0
+    close_count = 0
     state = ''
 
     while True:
         _, frame = cam.read()
-        frame = resize(frame, 400)
+        frame = resize(frame, 500)
 
-        # f_points = detectFace(frame)
-        # print(f_points, sep='\n')
-        
         eyes = eyesCrop(frame)
         if eyes is None:
             continue
         else:
-            left_eye, right_eye = eyes
+            eye1, eye2 = eyes
 
-        prediction = (model.predict(cnnPreprocess(left_eye)) + model.predict(cnnPreprocess(right_eye)))/2.0
-        # print(prediction)
-        
-        # blinks
-		# if the eyes are open reset the counter for close eyes
+        if eye2 is None:
+            cv2.imshow('Eye1', eye1)
+            prediction = model.predict(cnnPreprocess(eye1))
+
+        else:
+            cv2.imshow('Eye2', eye2)
+            prediction = (model.predict(cnnPreprocess(eye1)) +
+                          model.predict(cnnPreprocess(eye2)))/2.0
+
         if prediction > 0.5:
             state = 'Open'
             close_count = 0
@@ -39,20 +38,15 @@ def main():
             state = 'Closed'
             close_count += 1
 
-		# if the eyes are open and previously were closed
-		# for sufficient no. of frames then increcement the total blinks
-        if state == 'Open' and mem_count > 1:
-            blink_count += 1
-		# keep the counter for the next loop 
-        mem_count = close_count
-
-		# draw the total number of blinks on the frame along with
-		# the state for the frame
-        cv2.putText(frame, "Blinks: {}".format(blink_count), (10, 30),
-			cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "Blinks: {}".format(close_count), (10, 30),
+                    cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.7, (0, 0, 255), 2)
         cv2.putText(frame, "Eyes: {}".format(state), (300, 30),
-			cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.7, (0, 0, 255), 2)
-		
+                    cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.7, (0, 0, 255), 2)
+
+        if close_count > 10:
+            cv2.putText(frame, "ALERT", (100, 50),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.7, (0, 0, 255), 2)
+
         cv2.imshow('Drowsiness Detector', frame)
 
         key = cv2.waitKey(1) & 0xFF
